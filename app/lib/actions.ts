c9ebec37bc4d4e7bb53370b522dffbfb2baeb20a
GRZ29 <rosalesgerald198@gmail.com>
2024-07-02 16:58:51 -0600

    chapter16

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -4,6 +4,17 @@ import { z } from 'zod';
 import { sql } from '@vercel/postgres';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
+import { signIn } from '@/auth';
+import { AuthError } from 'next-auth';
+
+export type State = {
+    errors?: {
+        customerId?: string[];
+        amount?: string[];
+        status?: string[];
+    };
+    message?: string | null;
+};
 
 const FormSchema = z.object({
     id: z.string(),
@@ -132,11 +143,21 @@ export async function deleteInvoice(id: string) {
     }
 }
 
-export type State = {
-    errors?: {
-        customerId?: string[];
-        amount?: string[];
-        status?: string[];
-    };
-    message?: string | null;
-};
+export async function authenticate(
+    prevState: string | undefined,
+    formData: FormData,
+) {
+    try {
+        await signIn('credentials', formData);
+    } catch (error) {
+        if (error instanceof AuthError) {
+            switch (error.type) {
+                case 'CredentialsSignin':
+                    return 'Invalid credentials.';
+                default:
+                    return 'Something went wrong.';
+            }
+        }
+        throw error;
+    }
+}
